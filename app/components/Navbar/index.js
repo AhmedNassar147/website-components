069073f5d56193b94ed95ab997381972239434c8
Ahmed <nassarahmed750@gmail.com>
2018-01-22 22:52:55 +0200

    prepare responsive navbar

---
+++ app/components/Navbar/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Friends</li>
+        <li>Service</li>
+        <li>Team</li>
+      </ul>
+    </nav>
+  );
+}
+
+Navbar.propTypes = {
+
+};
+
+export default Navbar;
---
+++ app/containers/HomePage/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
+import makeSelectHomePage from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+import Navbar from '../../components/Navbar/index';
+
+export class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div >
+        <div>
+          <Navbar />
+        </div>
+      </div>
+    );
+  }
+}
+
+HomePage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  homepage: makeSelectHomePage(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+const withReducer = injectReducer({ key: 'homePage', reducer });
+const withSaga = injectSaga({ key: 'homePage', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(HomePage);
