02a1d64952bdc96d2d79479da987e5dd86e19e19
Ahmed <nassarahmed750@gmail.com>
2018-01-23 20:49:05 +0200

    design navbar

--- app/components/Navbar/index.js
+++ app/components/Navbar/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './nav.css';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>Friends</li>
-        <li>Service</li>
-        <li>Team</li>
-      </ul>
-    </nav>
+    <header>
+      <div id="top-wrapper">
+        <div id="logo"><Link to="#" style={linkLogoStyle}>Logo</Link></div>
+        <nav>
+          <span>&#9776;</span>
+          <ul>
+            <li><Link to="#" style={LinkStyle}>Home</Link></li>
+            <li><Link to="#" style={LinkStyle}>Category</Link></li>
+            <li><Link to="#" style={LinkStyle}>Shop</Link></li>
+            <li><Link to="#" style={LinkStyle}>Friends</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
+const LinkStyle = {
+  display: 'block',
+  width: '90px',
+  fontSize: '20px',
+  color: 'white',
+  textDecoration: 'none',
+};
+
+const linkLogoStyle = {
+  color: 'black',
+  textDecoration: 'none',
+};
+
 
 Navbar.propTypes = {
 
