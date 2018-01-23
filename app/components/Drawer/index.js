37571d7d5d9cfcb93be131d45b6940c008963707
Ahmed <nassarahmed750@gmail.com>
2018-01-24 01:17:07 +0200

    navbar styles

---
+++ app/components/Drawer/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './drawer.css';
+
+function Drawer() {
+  return (
+    <div>
+      <section id="drawer">
+        <div className="drawer-bck-img">
+          <ul>
+            <li>imag</li>
+            <li>name</li>
+            <li>email</li>
+          </ul>
+        </div>
+      </section>
+      <section>
+        <div>
+          <p>daasda</p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+Drawer.propTypes = {
+
+};
+
+export default Drawer;
--- app/components/Navbar/index.js
+++ app/components/Navbar/index.js
@@ -3,31 +3,31 @@ import { Link } from 'react-router-dom';
 import './nav.css';
 
 function Navbar() {
+  const userPicUrl = 'ttps://images.pexels.com/photos/819848/pexels-photo-819848.jpeg?w=940&h=650&auto=compress&cs=tinysrgb';
   return (
     <header>
       <div id="top-wrapper">
-        <div id="logo"><Link to="#" style={linkLogoStyle}>Logo</Link></div>
-        <nav>
-          <span>&#9776;</span>
+        <nav className="left">
           <ul>
-            <li><Link to="#" style={LinkStyle}>Home</Link></li>
-            <li><Link to="#" style={LinkStyle}>Category</Link></li>
-            <li><Link to="#" style={LinkStyle}>Shop</Link></li>
-            <li><Link to="#" style={LinkStyle}>Friends</Link></li>
+            <li><span>&#9776;</span></li>
+            <li id="logo"><Link to="#" style={linkLogoStyle}><h3>SiteTitle</h3></Link></li>
+          </ul>
+        </nav>
+        <nav className="right">
+          <img src={userPicUrl} alt="userMob-img" className="avatar" id="user-mob-img" />
+          <ul>
+            <li><input type="search" id="search" placeholder="search" /></li>
+            <li><Link to="#" className="link">Home</Link></li>
+            <li><Link to="#" className="link">Category</Link></li>
+            <li><Link to="#" className="link">Shop</Link></li>
+            <li><Link to="#" className="link">Friends</Link></li>
+            <li><img src={userPicUrl} alt="user-img" className="avatar" /></li>
           </ul>
         </nav>
       </div>
     </header>
   );
 }
-const LinkStyle = {
-  display: 'block',
-  width: '90px',
-  fontSize: '20px',
-  color: 'white',
-  textDecoration: 'none',
-};
-
 const linkLogoStyle = {
   color: 'black',
   textDecoration: 'none',
